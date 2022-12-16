c6320a4da5ecb84ce89bead85162e57fc405b2cc
Isuru Udantha <104246945+IUdantha@users.noreply.github.com>
2022-12-17 00:38:24 +0530

    Add files via upload

---
+++ java/in/inquiry/inquiry.java
@@ -0,0 +1,102 @@
+package in.inquiry;
+
+
+public class inquiry {
+
+	
+	private String name;
+	private String email;
+	private String number;
+	private String faculty;
+	private String year;
+	private String inquirytype;
+	private String message;
+	private String filename;
+
+
+	
+	public inquiry(String name, String email, String number, String faculty, String year, String inquirytype,
+			String message, String filename) {
+		this.name = name;
+		this.email = email;
+		this.number = number;
+		this.faculty = faculty;
+		this.year = year;
+		this.inquirytype = inquirytype;
+		this.message = message;
+		this.filename = filename;
+		
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public String getNumber() {
+		return number;
+	}
+
+
+
+
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+
+
+
+
+
+	public String getYear() {
+		return year;
+	}
+
+
+
+
+
+	public String getInquirytype() {
+		return inquirytype;
+	}
+
+
+
+	
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+
+
+
+	public String getFilename() {
+		return filename;
+	}
+
+	
+	}
+
+
+		
+
+
+
+
+
+
+	
